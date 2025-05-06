7a945f2f04346df3ab69e395d0ebbc6e0c8b88ba
Abhinav Tushar <abhinav@lepisma.xyz>
2025-05-06 13:51:24 +0530

    Add simple file content loader in tabs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
   import { browser } from '$app/environment';
   import { loadProfiles, loadLog } from '$lib/fs';
+  import { Tabs } from '@skeletonlabs/skeleton-svelte';
+  import IconUser from '@lucide/svelte/icons/user';
 
   let profiles = $state([]);
+  let selected = $state(null);
   let logs = $state([]);
 
   if (browser) {
@@ -10,23 +13,29 @@
     directoryInput.addEventListener('change', async (event) => {
       profiles = await loadProfiles(event.target.files);
       logs = await Promise.all(profiles.map(async (p) => await loadLog(p)));
+      selected = profiles[0].name;
     });
   }
+
 </script>
 
 <h1 class="text-lg">harp</h1>
-
 <input type="file" id="root-dir" webkitdirectory directory>
 
-<ul class="list-disc">
+<Tabs value={selected} onValueChange={(e) => (selected = e.value)}>
+  {#snippet list()}
   {#each profiles as profile}
-    <li>{profile.name}</li>
+    <Tabs.Control value={ profile.name }>
+      {#snippet lead()}<IconUser size={20} />{/snippet}
+      { profile.name }
+    </Tabs.Control>
   {/each}
-</ul>
-
-{#each logs as log}
-  <hr>
-  <pre>
-    { log }
-  </pre>
-{/each}
+  {/snippet}
+    {#snippet content()}
+    {#each profiles as profile, i}
+      <Tabs.Panel value={ profile.name }>
+        <textarea readonly>{ logs[i] }</textarea>
+      </Tabs.Panel>
+    {/each}
+    {/snippet}
+</Tabs>
