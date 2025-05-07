9495c03a9048863dc276960157d0a216f3e013e8
Abhinav Tushar <abhinav@lepisma.xyz>
2025-05-07 15:20:37 +0530

    Add types for the first version

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -2,6 +2,65 @@
 export interface Profile {
   uuid: string;
   name: string;
-  dir: string,
-  logFile: File;
+  file: File;
+}
+
+export interface Metric {
+  id: string;
+  unit: string;
+  tags: string[];
+  range?: [number, number];
+  healthyRange?: [number, number];
+}
+
+export interface Asset {
+  fileName: string;
+  mimeType?: string;
+  text?: string;
+}
+
+export interface MetricValue {
+  id: string;
+  value: number;
+}
+
+export interface JournalEntry {
+  datetime: Date;
+  uuid: string;
+  tags: string[];
+  metrics: MetricValue[];
+  text: string;
+  assets: Asset[];
+  isPrivate: boolean;
+}
+
+export interface Journal {
+  name: string;
+  entries: JournalEntry[];
+}
+
+export interface Source {
+  id: string;
+  description: string;
+}
+
+export interface Report {
+  name: string;
+  datetime: Date;
+  uuid: string;
+  tags: string[];
+  source: Source;
+  assets: Asset[];
+  metrics: MetricValue[];
+  annotation?: string;
+}
+
+export interface Consultation {
+  name: string;
+  datetime: Date;
+  uuid: string;
+  tags: string[];
+  source: Source;
+  assets: Asset[];
+  annotation?: string;
 }
