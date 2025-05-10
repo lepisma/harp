b344ea7c3c18384d1df6ef34a83128769f5e8f64
Abhinav Tushar <abhinav@lepisma.xyz>
2025-05-10 10:42:31 +0530

    Fix bug with db -> ops refactoring

--- src/routes/profile/[uuid]/+page.svelte
+++ src/routes/profile/[uuid]/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { page } from '$app/stores';
-  import { loadDB, loadProfile, saveProfile } from '$lib/db';
+  import { loadDB } from '$lib/db';
+  import { loadProfile, saveProfile } from '$lib/ops';
   import { Tabs } from '@skeletonlabs/skeleton-svelte';
   import IconNotepadText from '@lucide/svelte/icons/notepad-text';
   import IconChartScatter from '@lucide/svelte/icons/chart-scatter';
@@ -13,7 +14,7 @@
   import IconScrollText from '@lucide/svelte/icons/scroll-text';
   import { onMount } from 'svelte';
   import type { JournalEntry } from '$lib/types';
-  import { newProfile, profileTags } from '$lib/utils';
+  import { profileTags } from '$lib/utils';
 
   import JournalForm from '$lib/components/journal-form.svelte';
   import JournalEntryCard from '$lib/components/journal-entry-card.svelte';
@@ -21,10 +22,10 @@
 
   let profileId = $page.params.uuid;
 
-  let db = $state({});
-  let profile = $state(newProfile(undefined));
-  let journal = $derived(profile.journals[0]);
-  let tags = $derived(profileTags(profile));
+  let db = $state(null);
+  let profile = $state(null);
+  let journal = $derived(profile !== null ? profile.journals[0] : []);
+  let tags = $derived(profile !== null ? profileTags(profile) : []);
 
   let selectedTab = $state('journal');
   let isJournalFormOpen = $state(false);
@@ -84,98 +85,100 @@
 
 </script>
 
-<div class="grid grid-rows-[auto_1fr_auto]">
-  <div class="container mx-auto grid grid-cols-1 xl:grid-cols-[200px_minmax(0px,_1fr)_200px]">
-    <header class="mt-3">
-      <a href="/"><div class="h3 text-gray-400 pt-3 pl-3"><i>harp</i></div></a>
-      <h1 class="h1 p-3">{profile.name}</h1>
-    </header>
+{#if profile !== null}
+  <div class="grid grid-rows-[auto_1fr_auto]">
+    <div class="container mx-auto grid grid-cols-1 xl:grid-cols-[200px_minmax(0px,_1fr)_200px]">
+      <header class="mt-3">
+        <a href="/"><div class="h3 text-gray-400 pt-3 pl-3"><i>harp</i></div></a>
+        <h1 class="h1 p-3">{profile.name}</h1>
+      </header>
 
-    <main class="col-span-1 p-4">
-      <div class="mb-5 flex items-center justify-between gap-2">
-        <div>
-          <button type="button" disabled class="btn btn-sm preset-filled">
-            <span>Share Selection</span>
-            <IconScrollText size={14} />
-          </button>
+      <main class="col-span-1 p-4">
+        <div class="mb-5 flex items-center justify-between gap-2">
+          <div>
+            <button type="button" disabled class="btn btn-sm preset-filled">
+              <span>Share Selection</span>
+              <IconScrollText size={14} />
+            </button>
+          </div>
+          <div>
+            <button type="button" onclick={exportProfile} class="btn btn-sm preset-filled">
+              <span>Export</span>
+              <IconDownload size={14} />
+            </button>
+            <button type="button" disabled class="btn btn-sm preset-outlined">
+              <span>Import</span>
+              <IconUpload size={14} />
+            </button>
+          </div>
         </div>
-        <div>
-          <button type="button" onclick={exportProfile} class="btn btn-sm preset-filled">
-            <span>Export</span>
-            <IconDownload size={14} />
-          </button>
-          <button type="button" disabled class="btn btn-sm preset-outlined">
-            <span>Import</span>
-            <IconUpload size={14} />
-          </button>
+
+        <hr class="text-gray-300">
+        <div class="my-4">
+          <p class="text-semibold flex gap-2 mb-4"><IconFunnelPlus /><span>Filter by tags</span></p>
+
+          <small class="opacity-60">
+            {#each tags as tag}
+              <span class="inline-block bg-gray-200 rounded-md px-2 py-1 text-sm font-semibold text-gray-700 mr-2">#{tag}</span>
+            {/each}
+          </small>
         </div>
-      </div>
+        <hr class="text-gray-300">
 
-      <hr class="text-gray-300">
-      <div class="my-4">
-        <p class="text-semibold flex gap-2 mb-4"><IconFunnelPlus /><span>Filter by tags</span></p>
+        <div class="mt-5">
+          <Tabs value={selectedTab} onValueChange={(e) => (selectedTab = e.value)}>
+            {#snippet list()}
+            <Tabs.Control value="journal">
+              {#snippet lead()}<IconNotepadText size={20} />{/snippet}
+              Journal
+            </Tabs.Control>
+            <Tabs.Control value="metrics">
+              {#snippet lead()}<IconChartScatter size={20} />{/snippet}
+              Metrics
+            </Tabs.Control>
+            <Tabs.Control value="reports">
+              {#snippet lead()}<IconScanHeart size={20} />{/snippet}
+              Reports
+            </Tabs.Control>
+            <Tabs.Control value="consultations">
+              {#snippet lead()}<IconClipboardPen size={20} />{/snippet}
+              Consultations
+            </Tabs.Control>
+            {/snippet}
+            {#snippet content()}
+            <Tabs.Panel value="journal">
+              <div class="mb-4">
+                <button type="button" onclick={() => isJournalFormOpen = true} class="btn btn-sm preset-outlined">
+                  <span>New Entry</span>
+                  <IconPlus size={18} />
+                </button>
+              </div>
 
-        <small class="opacity-60">
-          {#each tags as tag}
-            <span class="inline-block bg-gray-200 rounded-md px-2 py-1 text-sm font-semibold text-gray-700 mr-2">#{tag}</span>
-          {/each}
-        </small>
-      </div>
-      <hr class="text-gray-300">
+              {#if isJournalFormOpen}
+                <JournalForm onSave={handleNewJournalEntry} onClose={() => isJournalFormOpen = false} />
+              {/if}
 
-      <div class="mt-5">
-        <Tabs value={selectedTab} onValueChange={(e) => (selectedTab = e.value)}>
-          {#snippet list()}
-          <Tabs.Control value="journal">
-            {#snippet lead()}<IconNotepadText size={20} />{/snippet}
-            Journal
-          </Tabs.Control>
-          <Tabs.Control value="metrics">
-            {#snippet lead()}<IconChartScatter size={20} />{/snippet}
-            Metrics
-          </Tabs.Control>
-          <Tabs.Control value="reports">
-            {#snippet lead()}<IconScanHeart size={20} />{/snippet}
-            Reports
-          </Tabs.Control>
-          <Tabs.Control value="consultations">
-            {#snippet lead()}<IconClipboardPen size={20} />{/snippet}
-            Consultations
-          </Tabs.Control>
-          {/snippet}
-          {#snippet content()}
-          <Tabs.Panel value="journal">
-            <div class="mb-4">
-              <button type="button" onclick={() => isJournalFormOpen = true} class="btn btn-sm preset-outlined">
-                <span>New Entry</span>
-                <IconPlus size={18} />
-              </button>
-            </div>
-
-            {#if isJournalFormOpen}
-              <JournalForm onSave={handleNewJournalEntry} onClose={() => isJournalFormOpen = false} />
-            {/if}
-
-            <div class="grid gap-4 md:grid-cols-1">
-              {#each [...journal.entries].sort((a, b) => a.datetime < b.datetime) as entry}
-                <JournalEntryCard entry={ entry } onDelete={handleDeleteJournalEntry} onEdit={handleEditJournalEntry} />
-              {/each}
-            </div>
-          </Tabs.Panel>
-          <Tabs.Panel value="metrics">
-            Under Development
-          </Tabs.Panel>
-          <Tabs.Panel value="reports">
-            Under Development
-          </Tabs.Panel>
-          <Tabs.Panel value="consultations">
-            Under Development
-          </Tabs.Panel>
-          {/snippet}
-        </Tabs>
-      </div>
-    </main>
+              <div class="grid gap-4 md:grid-cols-1">
+                {#each [...journal.entries].sort((a, b) => a.datetime < b.datetime) as entry}
+                  <JournalEntryCard entry={ entry } onDelete={handleDeleteJournalEntry} onEdit={handleEditJournalEntry} />
+                {/each}
+              </div>
+            </Tabs.Panel>
+            <Tabs.Panel value="metrics">
+              Under Development
+            </Tabs.Panel>
+            <Tabs.Panel value="reports">
+              Under Development
+            </Tabs.Panel>
+            <Tabs.Panel value="consultations">
+              Under Development
+            </Tabs.Panel>
+            {/snippet}
+          </Tabs>
+        </div>
+      </main>
+    </div>
+    <footer>
+    </footer>
   </div>
-  <footer>
-  </footer>
-</div>
+{/if}
